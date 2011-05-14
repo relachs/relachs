ddf3ebb83653d093feac9b9d75d797e2c5aad30d
relachs <relachs@ubuntu.(none)>
2011-05-14 12:54:28 -0700

    init :-)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Relachs::Application.config.secret_token = 'bc555c1e71a2263413759654d8310757d3344876b8a06d59246af659d558aaf57e5d866238c1748ece04c9bac070b793d660d6b4760e2680f4866c0dac0043f0'
